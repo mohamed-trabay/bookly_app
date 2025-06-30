7b5f7f8a5c4d347b325fa92f5125db082bb15ae1
mohamed-trabay <new-email@example.com>
2025-06-30 05:09:45 +0300

    adding sliding animation to splash body

---
+++ lib/features/splash/presentation/views/widgets/sliding_text.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SlidingText extends StatelessWidget {
+  const SlidingText({
+    super.key,
+    required AnimationController controller,
+  }) : _controller = controller;
+
+  final AnimationController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context,_) {
+        return  const Text(
+          'Read Free Books',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        );
+      },
+    );
+  }
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,18 +1,54 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    _animation = Tween<Offset>(
+      begin: const Offset(0, 4),
+      end: Offset.zero,
+    ).animate(_controller);
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xff100B20),
-      width: double.infinity,
-      height: double.infinity,
-      child: Center(
-          child:
-          Image.asset('assets/images/Logo.png',width: double.infinity,height: double.infinity,)
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+        const SizedBox(height: 8),
+        SlideTransition(
+          position: _animation,
+          child: SlidingText(controller: _controller),
+        ),
+      ],
     );
   }
 }

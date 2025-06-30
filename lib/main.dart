ed3f426f424d2beebd8c5106c1e7315cadd51888
mohamed-trabay <new-email@example.com>
2025-06-30 05:13:52 +0300

    feat: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return   MaterialApp(
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor:   kPrimaryColor,),
+      debugShowCheckedModeBanner: false,
+
+      home: const SplashView()
+
+
+
+    );
+  }
+}

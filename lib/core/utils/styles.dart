902ef4f9694b907e04a8da412611b294e13ff931
mohamed-trabay <new-email@example.com>
2025-07-08 20:56:00 +0300

    add font family

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 abstract class Styles {
   static const titleMedium = TextStyle(
-    fontSize: 22,
+    fontSize: 20,
     fontWeight: FontWeight.w600,
   );
 }

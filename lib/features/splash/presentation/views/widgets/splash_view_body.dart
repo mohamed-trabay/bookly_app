c5b82fd13a5785dd89fccb8b5462b1877f5486e0
mohamed-trabay <new-email@example.com>
2025-08-06 05:39:58 +0300

    create ServerFailure fromDioError

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -60,6 +60,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
   void navigateToHome() {
     Future.delayed(const Duration(seconds: 2), () {
+      // ignore: use_build_context_synchronously
       GoRouter.of(context).push(AppRouter.kHomeView);
     });
   }
